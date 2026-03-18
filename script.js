3e34e79238260d83ddef90ff0bab367e2086ec4f
Tacoblude <tacoblude@tuta.io>
2026-03-18 11:29:51 +0100

    updated website added products.html

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const info = document.getElementById("info");
 const btn = document.getElementById("btn");
+const prod = document.getElementById("prod");
+
 
 btn.addEventListener("click", () => {
   if (info.style.display === "none") {
@@ -9,3 +11,9 @@ btn.addEventListener("click", () => {
   }
 });
 
+prod.addEventListener("click", () => {
+  if (info.style.display === "none")
+  {window.location.href = "products.html";}
+  else
+  {window.location.href = "index.html";}
+});
